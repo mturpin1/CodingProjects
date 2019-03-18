3fc3a71effdbfae4e52dd6b4233a1e8c968315d6
Micah Turpin <MTurpin@Micahs-MacBook-Air.local>
2019-03-17 21:00:38 -0500

    more python stuff

--- Python/ppm.py
+++ Python/ppm.py
@@ -8,28 +8,36 @@ def canisters1(radius):
     turtle.penup()
     turtle.forward(2 * radius + 5)
 
-def moving():
-    turtle.goto(-56, 0)
+def movement():
+    turtle.goto(-62, 62)
 
 def canisters2(height, radius):
     turtle.pendown()
-    turtle.forward(radius * 2)
+    turtle.forward(int(radius) * 4)
     turtle.right(90)
     turtle.forward(height)
     turtle.right(90)
-    turtle.forward(radius * 2)
+    turtle.forward(int(radius) * 4)
     turtle.right(90)
     turtle.forward(height)
     turtle.right(90)
     turtle.penup()
-    turtle.forward(radius * 2 + 5)
+    turtle.forward(int(radius) * 4 + 5)
+
+def movement2():
+    turtle.goto(-50, 14)
 
 for x in range(10):
     canisters1(12)
 
-moving()
+movement()
 
 for x in range(10):
     canisters2(36, 6)
+
+movement2()
+
+for x in range(10):
+    canisters1(12)
 
 turtle.exitonclick()
