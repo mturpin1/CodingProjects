242a29712ad904badad0a820d9b1882b693ca3ed
Micah Turpin <MTurpin@Micahs-MacBook-Air.local>
2019-03-18 07:48:58 -0500

    trying to figure it out

--- Python/ppm.py
+++ Python/ppm.py
@@ -1,43 +1,21 @@
 import turtle
 turtle.speed('fastest')
+turtle.circle(305)
 turtle.penup()
-turtle.goto(-50, 50)
+turtle.goto(0, 305)
+turtle.left(135)
 def canisters1(radius):
+    turtle.forward()
     turtle.pendown()
+    turtle.begin_fill()
     turtle.circle(radius)
+    turtle.end_fill()
     turtle.penup()
-    turtle.forward(2 * radius + 5)
-
-def movement():
-    turtle.goto(-62, 62)
-
-def canisters2(height, radius):
-    turtle.pendown()
-    turtle.forward(int(radius) * 4)
-    turtle.right(90)
-    turtle.forward(height)
-    turtle.right(90)
-    turtle.forward(int(radius) * 4)
-    turtle.right(90)
-    turtle.forward(height)
-    turtle.right(90)
-    turtle.penup()
-    turtle.forward(int(radius) * 4 + 5)
-
-def movement2():
-    turtle.goto(-50, 14)
+    turtle.goto(0, 25)
+    turtle.right(20)
 
 for x in range(10):
     canisters1(12)
 
-movement()
-
-for x in range(10):
-    canisters2(36, 6)
-
-movement2()
-
-for x in range(10):
-    canisters1(12)
 
 turtle.exitonclick()
