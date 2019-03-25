893d3835d954e8b604f5174267e9def7ee0e1fe8
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-03-25 10:59:50 -0500

    stuff

---
+++ WebDesign/JavaScript/jQueryPractice1/script.js
@@ -0,0 +1,11 @@
+$('#Btn1').click(function() {
+    $('#Result1').html("Look who knows some JS y'all!")
+})
+
+$('#Btn2').click(function() {
+    let num1 = parseInt('#Result2')
+    let num2 = parseInt('#Result3')
+    let newSum = 
+    $('$Result4').html(`The sum is $('#newSum')`)
+})
+
