001535277669c5453f552b26bbb434fde4f638b3
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-03-25 11:46:54 -0500

    more stuff

--- WebDesign/JavaScript/jQueryPractice1/script.js
+++ WebDesign/JavaScript/jQueryPractice1/script.js
@@ -3,9 +3,16 @@ $('#Btn1').click(function() {
 })
 
 $('#Btn2').click(function() {
-    let num1 = parseInt('#Result2')
-    let num2 = parseInt('#Result3')
-    let newSum = 
-    $('$Result4').html(`The sum is $('#newSum')`)
+    let num1 = parseInt($('#Result2').val())
+    let num2 = parseInt($('#Result3').val())
+    let newSum = num1 + num2
+    $('#Result4').html(`The sum is ${newSum}`)
 })
 
+$('#Btn3').click(function() {
+    $('#Result5').html("Time to get swole")
+    $('#Result5').animate({
+        'font-size': +30,
+        'letter-spacing': +5
+    })
+})
