b3d01577cdfa2b3aea9a32709237762894a0ca4e
Micah Turpin <MTurpin@Micahs-MacBook-Air.local>
2019-03-27 16:18:49 -0500

    JS stuff, arghhhhh!

--- WebDesign/JavaScript/password/script.js
+++ WebDesign/JavaScript/password/script.js
@@ -8,8 +8,22 @@ function lengthGood() {
     }
 }
 
-function containsNum() {
-
+function containsNum(word) {
+    // for loop, check each character, if it's a number, break the loop word.charAt()
+    let hasNum
+    if (word === undefined) {
+        return false
+    } else {
+        for (i = 0; i < word.length; i++) {
+            if (isNaN(word.charAt(i)) ) {
+                hasNum = false
+            } else {
+                hasNum = true
+                break
+            }
+        }
+        return hasNum
+    }
 }
 
 function passMatch() {
@@ -29,5 +43,31 @@ function boxesFilled() {
 }
 
 $('#submitBoi').click(function() {
-
+    if (lengthGood()) {
+        $('#passwordLength').hide()
+    } else {
+        $('#passwordLength').show()
+    }
+    
+    if (containsNum()) {
+        $('#passwordContains').show()
+    } else {
+        $('#passwordContains').hide()
+    }
+    
+    if (passMatch()) {
+        $('#passwordMatch').hide()
+    } else {
+        $('#passwordMatch').show()
+    }
+    
+    if (boxesFilled()) {
+        $('#boxFill').hide()
+    } else {
+        $('#boxFill').show()
+    }
+    // if (all functions return whatever they're supposed to) { alert() }
 })
+if (lengthGood === true && containsNum === true && passMatch === true && boxesFilled === true) {
+    alert('Congratulations, you have successfully filled out the form. Thanks for signing up!')
+}
