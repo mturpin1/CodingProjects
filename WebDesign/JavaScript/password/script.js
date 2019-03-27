c005c36bf44783150f33d3ac3a663b2a5c5b762a
Micah Turpin <MTurpin@Micahs-MacBook-Air.local>
2019-03-27 10:13:48 -0500

    password checked stuff

--- WebDesign/JavaScript/password/script.js
+++ WebDesign/JavaScript/password/script.js
@@ -1,1 +1,18 @@
+function passwordMustContain() {
+    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for (i = 0; i < $('#password'.val().length(); i ++) {
+
+    }
+}
+function checkingStuff() {    
+    if ($('#fullName').val() === '' || $('#username').val() === '' || $('#password').val() === '' || $('#confirmPassword').val() === '') {
+        $('#boxFill').html('You forgot to fill in one or more boxes.')
+    } else if ($('#password').val() != $('#confirmPassword').val()) {
+        $('#passwordMatch').html('Your passwords must match.')
+    } else if ($('#password').val().length() < 8 || $('#confirmPassword').val().length() < 8) {
+        $('#passwordLength').html('Your password must be 8 or more characters.')
+    } else if ($('#password').val().length() < 8 || $('#confirmPassword').val().length() < 8) {
+        $('#passwordLength').html('Your password must be 8 or more characters.')
+    }
+}
 // if then statements go here //
