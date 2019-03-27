5c8a140ac173e218df62ea931da33caa6d7252da
Micah Turpin <MTurpin@Micahs-MacBook-Air.local>
2019-03-27 14:51:04 -0500

    mr mckee is da bomb

--- WebDesign/JavaScript/password/script.js
+++ WebDesign/JavaScript/password/script.js
@@ -1,18 +1,33 @@
-function passwordMustContain() {
-    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for (i = 0; i < $('#password'.val().length(); i ++) {
+$('.text-danger').hide()
 
+function lengthGood() {
+    if ($('#password').val().length >= 8) {
+        return true
+    } else {
+        return false
     }
 }
-function checkingStuff() {    
+
+function containsNum() {
+
+}
+
+function passMatch() {
+    if ($('#password').val() === $('#confirmPassword').val()) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function boxesFilled() {
     if ($('#fullName').val() === '' || $('#username').val() === '' || $('#password').val() === '' || $('#confirmPassword').val() === '') {
-        $('#boxFill').html('You forgot to fill in one or more boxes.')
-    } else if ($('#password').val() != $('#confirmPassword').val()) {
-        $('#passwordMatch').html('Your passwords must match.')
-    } else if ($('#password').val().length() < 8 || $('#confirmPassword').val().length() < 8) {
-        $('#passwordLength').html('Your password must be 8 or more characters.')
-    } else if ($('#password').val().length() < 8 || $('#confirmPassword').val().length() < 8) {
-        $('#passwordLength').html('Your password must be 8 or more characters.')
+        return false
+    } else {
+        return true
     }
 }
-// if then statements go here //
+
+$('#submitBoi').click(function() {
+
+})
