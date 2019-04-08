1065dc3e955985aa6c3ab895cb925dd90f706960
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-04-08 08:20:30 -0500

    tic tac toe

--- WebDesign/JavaScript/ticTacToe/script.js
+++ WebDesign/JavaScript/ticTacToe/script.js
@@ -9,3 +9,63 @@ let winArr = [
     ["#0-2", "#1-1", "#2-0"]        // forward slash
 ]
 
+let mark = 'X'
+let marksOnBoard = 0
+
+function markCell() {
+    if (!this.innerText) {
+        this.innerText = mark
+        marksOnBoard++
+        if (playerWon(mark)) {
+            displayMessage(`${mark} won the game! Click anywhere to reset!`)
+        } else if (marksOnBoard === 9) {
+            displayMessage('Tie Game! Click anywhere to reset!')
+        }
+        if (mark === 'X') {
+            mark = 'O'
+        } else if (mark === 'O') {
+            mark = 'X'
+        }
+    }
+}
+
+$('.cell').click(function() {
+    $('.cell').click(markCell)
+})
+
+function elementContains(id, mark) {
+    return $(id).text() === mark
+}
+
+function playerWon(mark) {
+    for (let i = 0; i < winArr.length; i++) {
+        let winCombo = winArr[i]
+        let won = winCombo.every(
+            function (id) {
+                return elementContains(id, mark)
+            }
+        )
+        if (won === true) return true
+    }
+    return false
+}
+
+function resetGame(msg) {
+    $('#message').html(`${msg}`)
+    $('#message').css('pointer-events', 'none')
+    marksOnBoard = 0
+    mark = 'X'
+    $('.cell').html('')
+    $('#message').html('')
+}
+
+function displayMessage(msg) {
+    $('#message').html(`${msg}`)
+    $('#message').css('pointer-events', 'all')
+}
+
+function listenForClickOnMessage() {
+    $('#message').click(resetGame)
+}
+
+listenForClickOnMessage()
