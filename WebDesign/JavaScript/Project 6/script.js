afb23885e1d1c9aaac85da3d689089e5e332397a
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-05-02 15:22:21 -0500

    finally got it working!!!!!!!!!!!!!!!!!!

--- WebDesign/JavaScript/Project 6/script.js
+++ WebDesign/JavaScript/Project 6/script.js
@@ -1,3 +1,3 @@
-$('.click').click(function){
-    
-}
+$('#triangleBoi').click(function() {
+    $('#triangleBoi').attr('animation', 'property: components.material.material.color; type: color;  to: red; dur: 1500;')
+})
