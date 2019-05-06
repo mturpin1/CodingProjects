d5d628baf612d6717a35f3f162c314feca1019d9
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-05-06 12:40:15 -0500

    pretty dang frustrated right now

--- WebDesign/JavaScript/Project 6/script.js
+++ WebDesign/JavaScript/Project 6/script.js
@@ -1,3 +1,4 @@
 $('#triangleBoi').click(function() {
     $('#triangleBoi').attr('animation', 'property: components.material.material.color; type: color;  to: red; dur: 1500;')
+    $('#yeet').attr('animation', 'property: components.material.material.opacity; type: opacity; to: 0.9; dur: 1500;')
 })
