2ab374030813a82f8bfda15c7c11a6b58db7960e
MICAH TURPIN <MICAH.TURPIN@nomma.lan>
2019-05-10 13:27:48 -0500

    stuff

--- WebDesign/JavaScript/Project 6/script.js
+++ WebDesign/JavaScript/Project 6/script.js
@@ -1,4 +1,11 @@
 $('#triangleBoi').click(function() {
-    $('#triangleBoi').attr('animation', 'property: components.material.material.color; type: color;  to: red; dur: 1500;')
-    $('#yeet').attr('animation', 'property: components.material.material.opacity; type: opacity; to: 0.9; dur: 1500;')
+    console.log('clicked!')
+    $('#triangleBoi').attr('animation', 'property: components.material.material.color; type: color; to: red; dur: 1500;');
+    $('#button').attr('animation', 'property: opacity;  to: 1; dur: 1500;');
+    $('#btnText').attr('animation', 'property: opacity;  to: 1; dur: 1500;');
+    console.log('yeet!')
+})
+
+$('#button').hover(function() {
+    $('#button').attr('animation', 'property: components.material.material.color; type: color; to: cyan; dur: 1500;')
 })
