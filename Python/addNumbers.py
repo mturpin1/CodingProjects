efa57ef866ffc738073f37593ed8790de9c65a70
MT <MTurpin@Micahs-MacBook-Air.local>
2020-06-25 22:31:15 -0400

    im gunna kill ms

--- Python/addNumbers.py
+++ Python/addNumbers.py
@@ -1,106 +1,110 @@
 import os
 import time
+os.system('clear')
+firstNumber = int(raw_input('Enter the first number:    ')) # denotes the first number that the user wants to use for any calculation
+result = int(firstNumber)
 def addition():
+    global result
     os.system('clear')
-    sum = firstNumber # sets the variable 'sum1' to the value of the variable 'firstNumber'
-    print sum, # prints the variable 'sum1'
+    print result, # prints the variable 'result'
     raw_input()
 
     while True:
         os.system('clear')
         number = raw_input('Enter the number you want to add:   ')
-        if number.lower() == 'sum':
-            print sum,
+        if number.lower() == 'result':
+            print result,
             raw_input()
-        elif number.isdigit(): 
-            sum += int(number)
-            os.system('clear')
         elif number.lower() == 'sub' or number.lower() == 'subtract' or number.lower() == '-':
             subtraction()
-        elif number.lower() == 'multiply' or number.lower() == '*' or number.lower() == 'x':
-            division()
+        elif number.lower() == 'multiply' or number.lower() == 'multiplication' or number.lower() == '*' or number.lower() == 'x':
+            multiplication()
         elif number.lower() == 'division' or number.lower() == 'divide' or number.lower() == '/':
             division()
-        else:
+        elif number.lower().islower():
             print('Let\'s try that again. . .')
             time.sleep(1.0)
-
+        else: 
+            result += int(number)
+            os.system('clear')
+        
 def subtraction():
+    global result
     os.system('clear')
-    difference = firstNumber # sets the variable 'difference' to the value of the variable 'firstNumber'
-    print difference, # prints the variable 'difference'
+    print result, # prints the variable 'result'
     raw_input() # used 
 
     while True:
         os.system('clear')
         number = raw_input('Enter the number you want to subtract:   ')
-        if number.lower() == 'difference':
-            print difference,
+        if number.lower() == 'result':
+            print result,
             raw_input()
-        elif number.isdigit(): 
-            difference -= int(number)
-            os.system('clear')
         elif number.lower() == 'add' or number.lower() == 'addition' or number.lower() == '+':
             addition()
-        elif number.lower() == 'multiply' or number.lower() == '*' or number.lower() == 'x':
+        elif number.lower() == 'multiply' or number.lower() == 'multiplication' or number.lower() == '*' or number.lower() == 'x':
             multiply()
         elif number.lower() == 'division' or number.lower() == 'divide' or number.lower() == '/':
             division()
-        else:
+        elif number.lower().islower():
             print('Let\'s try that again. . .')
             time.sleep(1.0)
-
+        else: 
+            result -= int(number)
+            os.system('clear')
+        
 def multiplication():
+    global result
     os.system('clear')
-    product = firstNumber # sets the variable 'product' to the value of the variable 'firstNumber'
-    print product, # prints the variable 'product'
+    print result, # prints the variable 'result'
     raw_input()
 
     while True:
         os.system('clear')
         number = raw_input('Enter the number you want to multiply:   ')
-        if number.lower() == 'product':
-            print product,
+        if number.lower() == 'result':
+            print result,
             raw_input()
-        elif number.isdigit(): 
-            product *= int(number)
-            os.system('clear')
         elif number.lower() == 'add' or number.lower() == 'addition' or number.lower() == '+':
             addition()
         elif number.lower() == 'sub' or number.lower() == 'subtraction' or number.lower() == '-':
             subtraction()
         elif number.lower() == 'division' or number.lower() == 'divide' or number.lower() == '/':
             division()
-        else:
+        elif number.lower().islower():
             print('Let\'s try that again. . .')
             time.sleep(1.0)
-
+        else: 
+            result *= int(number)
+            os.system('clear')
+        
 def division():
+    global result
     os.system('clear')
-    quotient = firstNumber # sets the variable 'quotient' to the value of the variable 'firstNumber'
-    print quotient, # prints the variable 'quotient'
+    print result, # prints the variable 'result'
     raw_input()
 
     while True:
         os.system('clear')
         number = raw_input('Enter the number you want to divide by:   ')
-        if number.lower() == 'quotient':
-            print quotient,
+        if number.lower() == 'result':
+            print result,
             raw_input()
-        elif number.isdigit(): 
-            quotient /= int(number)
-            os.system('clear')
         elif number.lower() == 'add' or number.lower() == 'addition' or number.lower() == '+':
             addition()
         elif number.lower() == 'sub' or number.lower() == 'subtraction' or number.lower() == '-':
             subtraction()
-        elif number.lower() == 'multiply' or number.lower() == '*' or number.lower() == 'x':
+        elif number.lower() == 'multiply' or number.lower() == 'multiplication' or number.lower() == '*' or number.lower() == 'x':
             multiplication()
-        else:
+        elif number.lower().islower():
             print('Let\'s try that again. . .')
             time.sleep(1.0)
-
-os.system('clear')
-firstNumber = int(raw_input('Enter the first number:    ')) # denotes the first number that the user wants to add
-function = raw_input('''What function would you like to perform? –type \'addition\', \'subtraction\', \'multiplication\', or \'division\'.
+        else: 
+            result /= int(number)
+            os.system('clear')
+        
+function_dict = {'addition':addition, 'subtraction':subtraction, 'multiplication':multiplication, 'division':division}
+func = raw_input('''What function would you like to perform? -type \'addition\', \'subtraction\', \'multiplication\', or \'division\'.
 Note: You can change this at any point throughout your use of this calculator:   ''').lower()
+
+function_dict[func]()
---
+++ Python/frustratedTesting.py
@@ -0,0 +1,4 @@
+import math, time, os
+os.system('clear')
+number = int(raw_input('You know what you\'re supposed to do here, dingus. . .'))
+print(type(number))
